22b02b1d0fc00be8034d2dba1fed99d40ae297ef
Rohit Kumar <rohitk@del.aithent.com>
2024-06-18 11:22:54 +0530

    Add String Question

---
+++ src/com/string/PalindromeString.java
@@ -0,0 +1,61 @@
+/*
+	Input: S = "abba"
+	Output: 1
+	Explanation: S is a palindrome
+ */
+package com.string;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+	
+	public static int palindromeString(String S) {
+        // code here
+        
+        int start=0;
+        int end=S.length()-1;
+        
+        while(start<end){
+            if(S.charAt(start)!=S.charAt(end)){
+                return 0;
+            }
+            start++;
+            end--;
+        }
+        return 1;
+        
+        /*
+        int start=0;
+        int end=S.length()-1;
+        char[] ch=S.toCharArray();
+        
+        while(start<end){
+            if(ch[start]!=ch[end]){
+                return 0;
+            }
+            start++;
+            end--;
+        }
+        return 1;*/
+        
+       /* String reverse="";
+        for(int i=S.length()-1; i>=0; i--){
+            reverse+=S.charAt(i);
+        }
+        if(S.equals(reverse)){
+            return 1;
+        }
+        else{
+            return 0;
+        }*/
+    }
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the String: ");
+		String str=sc.nextLine();
+		
+		System.out.println(palindromeString(str));
+	}
+}
---
+++ src/com/string/ReverseString.java
@@ -0,0 +1,51 @@
+/*
+	Input: s = Rohit
+	Output: tihoR
+*/
+package com.string;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	
+	public static String reverseString(String str)
+    {
+        int start=0;
+        int end=str.length()-1; 
+        char[] ch=str.toCharArray();
+        while(start<end){
+            char temp=ch[start];
+            ch[start]=ch[end];
+            ch[end]=temp;
+            start++;
+            end--;
+        }
+        
+        return new String(ch);
+        
+        /*String reverse="";
+        for(int i=str.length()-1; i>=0; i--){
+            reverse+=str.charAt(i);
+        }
+        
+        return reverse;*/
+        
+        /*StringBuilder sb=new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();*/
+        
+        /*StringBuffer sb=new StringBuffer(str);
+        sb.reverse();
+        return sb.toString();*/
+    }
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the String: ");
+		String str=sc.nextLine();
+		
+		System.out.println(reverseString(str));
+	}
+}
